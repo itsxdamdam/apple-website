1ec2f87b8fb22ce5138ca66e4b1ad7bd9c2811d3
Bakare Samuel Oluwadamilola <49197462+itsxdamdam@users.noreply.github.com>
2025-05-04 15:41:31 +0100

    complete 3D implementation

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,7 +1,14 @@
-import { OrbitControls, PerspectiveCamera, View } from "@react-three/drei";
+import {
+  Html,
+  OrbitControls,
+  PerspectiveCamera,
+  View,
+} from "@react-three/drei";
+import * as THREE from "three";
 import React, { Suspense } from "react";
 import Lights from "./Lights";
 import IPhone from "./IPhone";
+import Loader from "./Loader";
 
 const ModelView = ({
   index,
@@ -16,7 +23,9 @@ const ModelView = ({
     <View
       index={index}
       id={gsapType}
-      className={`w-full h-full ${index === 2} ? right-[-100%] : " "`}
+      className={`w-full h-full absolute ${
+        index === 2 ? "right-[-100%]" : " "
+      }`}
     >
       {/* Ambient Light */}
       <ambientLight intensity={0.3} />
@@ -30,7 +39,7 @@ const ModelView = ({
         enableZoom={false}
         enablePan={false}
         rotateSpeed={0.4}
-        // target={new THREE.Vector3(0, 0, 0)}
+        target={new THREE.Vector3(0, 0, 0)}
         onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
       />
 
@@ -39,7 +48,7 @@ const ModelView = ({
         name={`${index === 1} ? small : large`}
         position={[0, 0, 0]}
       >
-        <Suspense>
+        <Suspense fallback={<Loader />}>
           <IPhone
             scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
             item={item}
